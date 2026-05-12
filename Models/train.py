747e4359d714de9320805418df9d5554ff504b53
Pablo Valero <pablovaleromartinez@gmail.com>
2026-05-12 17:58:40 +0200

    Implemented train loop

---
+++ Models/train.py
@@ -0,0 +1,190 @@
+import torch
+
+from Models.gpt2 import GPTModel
+from Models.config import GPT_CONFIG_124M
+
+from Data.dataset_creation import create_dataloader
+import tiktoken
+
+from pathlib import Path
+
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+
+def token_ids_to_text(token_ids, tokenizer):
+    return tokenizer.decode(token_ids.squeeze(0).tolist())
+
+
+def generate_text(model, idx, max_new_tokens, context_size):
+    # Generate tokens one by one
+    for _ in range(max_new_tokens):
+        # Keep only the last context_size tokens because the model cannot process sequences longer than its context window
+        idx_cond = idx[:, -context_size:]
+
+        # Disable gradient computation during inference
+        with torch.no_grad():
+            # Forward pass through the model logits shape: (batch_size, seq_len, vocab_size)
+            logits = model(idx_cond)
+
+        # Select logits from the last generated token We only care about predicting the next token Shape:(batch_size, vocab_size)
+        logits = logits[:, -1, :]
+
+        # Convert logits into probabilities
+        probas = torch.softmax(logits, dim=-1)
+
+        # Select the token with highest probability Shape: (batch_size, 1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+        # Append the predicted token to the sequence Previous: (batch_size, seq_len) After concatenation:(batch_size, seq_len + 1)
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    # Return complete generated sequence
+    return idx
+
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+
+    logits = model(input_batch)
+
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(
+            0, 1
+        ),  # logits (batch_size, seq_len, voc_size) -> (predictions, voc_size)
+        target_batch.flatten(),  # Flatten target tokens into a single vector
+    )
+
+    return loss
+
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0
+    if len(data_loader) == 0:
+        return torch.nan
+    elif num_batches is None:
+        num_batches = len(
+            data_loader
+        )  # If no number of batches specified, iterates over all
+    else:
+        num_batches = min(num_batches, len(data_loader))
+
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i > num_batches:
+            break
+        loss = calc_loss_batch(input_batch, target_batch, model, device)
+        total_loss += loss.item()
+    return total_loss / num_batches
+
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()  # Puts the model in eval mode, disables dropouts
+    with torch.no_grad():  # Disables gradient tracking
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        validation_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, validation_loss
+
+
+def generate_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+
+    with torch.no_grad():
+        token_ids = generate_text(
+            model=model, idx=encoded, max_new_tokens=50, context_size=context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+
+def train(
+    model,
+    train_data_loader,
+    val_data_loader,
+    optimizer,
+    device,
+    epochs,
+    eval_freq,
+    eval_iter,
+    start_context,
+    tokenizer,
+):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(epochs):
+        model.train()  # Puts the model in train mode
+        for input_batch, targer_batch in train_data_loader:
+            optimizer.zero_grad()  # Reset loss gradients from previous batch train
+            loss = calc_loss_batch(input_batch, targer_batch, model, device)
+
+            loss.backward()  # Calculate loss gradients
+            optimizer.step()  # Update model weights
+
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            # Evaluation step
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_data_loader, val_data_loader, device, eval_iter
+                )
+                train_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+
+                print(
+                    f"Epoch:{epoch + 1} (Step {global_step:06d}): Train loss {train_loss:.3f} Validation loss {val_loss:.3f}"
+                )
+
+        generate_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
+
+if __name__ == "__main__":
+    file_path = Path(__file__).parent.parent / "Data" / "dracula.txt"
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        raw_text = file.read()
+
+    train_ratio = 0.9
+    split_idx = int(train_ratio * len(raw_text))
+
+    train_text = raw_text[:split_idx]
+    val_text = raw_text[split_idx:]
+
+    train_loader = create_dataloader(train_text)
+    val_loader = create_dataloader(val_text)
+    torch.manual_seed(123)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print(f"Using device: {device}")
+
+    model = GPTModel(GPT_CONFIG_124M)
+    model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+    epochs = 10
+
+    train_losses, val_losses, tokens_seen = train(
+        model,
+        train_loader,
+        val_loader,
+        optimizer,
+        device,
+        epochs=epochs,
+        eval_freq=5,
+        eval_iter=5,
+        start_context="Every effort moves you",
+        tokenizer=tokenizer,
+    )
